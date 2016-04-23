aaae0d46c03b0ae99ca96a667f831ed8722224c0
CallMeMeow <bastien.ravalet@gmail.com>
2016-04-23 02:01:43 +0200

    l

--- Feedify/src/main/java/Model/feed.java
+++ Feedify/src/main/java/Model/feed.java
@@ -9,11 +9,20 @@ package Model;
  *
  * @author Bastien
  */
-public class feed {
+public class Feed {
     private int id;
     private String name;
     private String url;
     private Boolean refresh_error;
+    private int new_articles;
+
+    public void setNew_articles(int new_articles) {
+        this.new_articles = new_articles;
+    }
+
+    public int getNew_articles() {
+        return new_articles;
+    }
 
     public void setId(int id) {
         this.id = id;
@@ -23,7 +32,7 @@ public class feed {
         this.name = name;
     }
 
-    public void setUtl(String url) {
+    public void setUrl(String url) {
         this.url = url;
     }
 
@@ -39,11 +48,38 @@ public class feed {
         return name;
     }
 
-    public String getUtl() {
+    public String getUrl() {
         return url;
     }
 
     public Boolean getRefresh_error() {
         return refresh_error;
-    }   
+    }
+    
+    public static class FeedPost
+    {
+        private String  name;
+        private String  url;
+
+        public FeedPost(String name, String url) {
+            this.name = name;
+            this.url = url;
+        }
+        
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+        
+        public String getName() {
+            return name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+    }
 }
